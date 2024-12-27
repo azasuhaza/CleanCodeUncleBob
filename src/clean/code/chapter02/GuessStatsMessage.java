ad9d197849b997d329f1e3a5e0e8249d4f623b0f
azasuhaza <azasuhaza@gmail.com>
2024-12-27 23:40:09 +0800

    new clean up class

--- src/clean/code/chapter02/GuessStatsMessage.java
+++ src/clean/code/chapter02/GuessStatsMessage.java
@@ -1,30 +1,41 @@
 package clean.code.chapter02;
 
-public class PrintGuessStatistics {
+public class GuessStatsMessage {
 
-  @SuppressWarnings("unused")
-  private void printGuessStats(char candidate, int count) {
-    String number;
-    String verb;
-    String pluralModifier;
-    
-    if(count ==0) {
-      number ="no";
-      verb= "are";
-      pluralModifier="s";
-    } else if (count==1) {
-      number ="1";
-      verb= "is";
-      pluralModifier="";      
-    } else {
-      number= Integer.toString(count);
-      verb= "are";
-      pluralModifier="s";        
-    }
-    String guessMessage = String.format(
-        "There %s %s %s %s", verb, number, candidate, pluralModifier
-        );
-    
-    System.out.print(guessMessage);
+  private String number;
+  private String verb;
+  private String pluralModifier;
+  
+  public String printGuessMessage(char candidate, int count) {
+    createMsgPart(count);
+    return String.format(
+        "There %s %s %s %s", verb, number, candidate, pluralModifier);
+  }
+  
+  private void createMsgPart(int count) {
+    if(count==0)
+      populateNoLetter();
+    else if(count==1)
+      populateOneLetter();
+    else
+      populateManyLetter(count);    
+  }
+
+  private void populateManyLetter(int count) {
+    number= Integer.toString(count);
+    verb= "are";
+    pluralModifier="s";   
+  }
+
+  private void populateOneLetter() {
+    number ="1";
+    verb= "is";
+    pluralModifier=""; 
+  }
+
+  private void populateNoLetter() {
+    number ="no";
+    verb= "are";
+    pluralModifier="s";
   }
 }
